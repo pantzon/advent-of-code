7517883ab8e0c2feb7f5934a32c272b36a3113ab
Pantz <pantz@pantz.online>
2024-12-06 21:19:42 -0500

    2015,2020 - Day 5

--- 2015/day5.ts
+++ 2015/day5.ts
@@ -2,9 +2,42 @@ import { readFileSync } from "fs";
 
 const data = readFileSync("./inputs/day5.txt", "utf-8");
 
-function part1() {}
+const p1rule1Regex = /[aeiou]/g;
+function p1rule1(val: string): boolean {
+  const result = val.match(p1rule1Regex);
+  return !!result && result.length > 2;
+}
 
-function part2() {}
+function p1rule2(val: string): boolean {
+  return !!val.split("").find((v, i, arr) => v == arr[i + 1]);
+}
+
+const p1rule3Regex = /ab|cd|pq|xy/;
+function p1rule3(val: string): boolean {
+  return !p1rule3Regex.test(val);
+}
+
+function part1() {
+  const nice = data
+    .split("\n")
+    .filter((v) => p1rule1(v) && p1rule2(v) && p1rule3(v));
+  console.log(`P1 Nice: ${nice.length}`);
+}
+
+function p2rule1(val: string): boolean {
+  return !!val
+    .split("")
+    .find((_, i) => val.indexOf(val.slice(i, i + 2), i + 2) > 0);
+}
+
+function p2rule2(val: string): boolean {
+  return !!val.split("").find((v, i, arr) => v == arr[i + 2]);
+}
+
+function part2() {
+  const nice = data.split("\n").filter((v) => p2rule1(v) && p2rule2(v));
+  console.log(`P1 Nice: ${nice.length}`);
+}
 
 console.log("DAY 5");
 part1();
--- 2020/day5.py
+++ 2020/day5.py
@@ -3,12 +3,21 @@ with open("inputs/day5.txt", "r") as f:
     data = f.read()
 
 
+data = set(
+    int("".join(["0" if c == "F" or c == "L" else "1" for c in s]), 2)
+    for s in data.split("\n")
+)
+
+
 def Part1():
-    pass
+    print("Max Seat: {}".format(max(*data)))
 
 
 def Part2():
-    pass
+    for i in range(min(*data) + 1, max(*data)):
+        if i not in data:
+            print("My Seat: {}".format(i))
+            break
 
 
 def main():
