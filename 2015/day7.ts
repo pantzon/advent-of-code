259990cf66525a82b601373344aa1ffacd9f62b5
Pantz <pantz@pantz.online>
2024-12-07 16:31:57 -0500

    2015,2020 - Day 7

--- 2015/day7.ts
+++ 2015/day7.ts
@@ -2,9 +2,109 @@ import { readFileSync } from "fs";
 
 const data = readFileSync("./inputs/day7.txt", "utf-8");
 
-function part1() {}
+const instructionRe = /^(?<input>.+) -> (?<output>[a-z]+)$/;
+const twoInputRe =
+  /^(?<input1>[a-z0-9]+) (?<cmd>AND|OR|LSHIFT|RSHIFT) (?<input2>[a-z0-9]+)$/;
+const oneInputRe = /^(?<cmd>NOT) (?<input1>[a-z0-9]+)$/;
+enum Cmd {
+  SET = "SET",
+  AND = "AND",
+  OR = "OR",
+  NOT = "NOT",
+  LSHIFT = "LSHIFT",
+  RSHIFT = "RSHIFT",
+}
+interface Instruction {
+  command: Cmd;
+  inputs: string[];
+}
 
-function part2() {}
+const instructions: Record<string, Instruction> = {};
+for (const line of data.split("\n")) {
+  const match = instructionRe.exec(line);
+  if (match && match.groups) {
+    const inputGroups = twoInputRe.exec(match.groups["input"])?.groups ||
+      oneInputRe.exec(match.groups["input"])?.groups || {
+        input1: match.groups["input"],
+      };
+    instructions[match.groups["output"]] = {
+      command: inputGroups["cmd"] ? (inputGroups["cmd"] as Cmd) : Cmd.SET,
+      inputs: inputGroups["input2"]
+        ? [inputGroups["input1"], inputGroups["input2"]]
+        : [inputGroups["input1"]],
+    };
+  }
+}
+
+function findValue(wire: string, cache: Record<string, number>): number {
+  const inst = instructions[wire];
+  if (!inst) {
+    console.log(`Unknown wire: ${wire}`);
+    return -1;
+  }
+  if (cache[wire] < 0) {
+    console.log(`Circular dependency! ${wire}`);
+    return -1;
+  }
+  if (cache[wire]) {
+    return cache[wire];
+  }
+  cache[wire] = -1;
+  let retVal = 0;
+  switch (inst.command) {
+    case Cmd.SET:
+      retVal = processInput(inst.inputs[0], cache);
+      break;
+    case Cmd.NOT:
+      retVal = ~processInput(inst.inputs[0], cache);
+      break;
+    case Cmd.AND:
+      retVal =
+        processInput(inst.inputs[0], cache) &
+        processInput(inst.inputs[1], cache);
+      break;
+    case Cmd.OR:
+      retVal =
+        processInput(inst.inputs[0], cache) |
+        processInput(inst.inputs[1], cache);
+      break;
+    case Cmd.LSHIFT:
+      retVal =
+        processInput(inst.inputs[0], cache) <<
+        processInput(inst.inputs[1], cache);
+      break;
+    case Cmd.RSHIFT:
+      retVal =
+        processInput(inst.inputs[0], cache) >>
+        processInput(inst.inputs[1], cache);
+      break;
+    default:
+      console.log(`Unknown Instruction: ${inst}`);
+      return -1;
+  }
+  cache[wire] = uint16(retVal);
+  return cache[wire];
+}
+
+function uint16(v: number) {
+  return v & 0xffff;
+}
+
+function processInput(input: string, cache: Record<string, number>): number {
+  if (input in instructions) {
+    return findValue(input, cache);
+  }
+  return uint16(parseInt(input));
+}
+
+function part1() {
+  console.log(`Original A: ${findValue("a", {})}`);
+}
+
+function part2() {
+  instructions["b"] = { command: Cmd.SET, inputs: [`${findValue("a", {})}`] };
+  console.log(`Recalc'd A: ${findValue("a", {})}`);
+}
 
 console.log("DAY 7");
 part1();
--- 2020/day7.py
+++ 2020/day7.py
@@ -1,14 +1,61 @@
+import re
+
 data = ""
 with open("inputs/day7.txt", "r") as f:
     data = f.read()
 
+BAG_RE = re.compile(r"^(?P<count>[0-9]+) (?P<color>[a-z ]+) bags?$")
+RULE_RE = re.compile(r"^(?P<color>[a-z ]+) bags contain (?P<bags>[0-9a-z, ]+)\.$")
+
+bag_groupings: dict[str, dict[str, int]] = {}
+bag_holdings: dict[str, dict[str, int]] = {}
+for line in data.split("\n"):
+    rule = RULE_RE.match(line)
+    if rule is None:
+        print("> ERROR: Bad rule! {}".format(line))
+    else:
+        outer_bag = rule.group("color")
+        inner_bags = rule.group("bags")
+        if inner_bags != "no other bags":
+            bag_holdings[outer_bag] = {}
+            for b in inner_bags.split(", "):
+                bag_match = BAG_RE.match(b)
+                if bag_match is None:
+                    print("> ERROR: Bad bag! {} ({})".format(b, line))
+                else:
+                    count = int(bag_match.group("count"))
+                    inner_bag = bag_match.group("color")
+                    if inner_bag not in bag_groupings:
+                        bag_groupings[inner_bag] = {}
+                    bag_groupings[inner_bag][outer_bag] = count
+                    bag_holdings[outer_bag][inner_bag] = count
+
+
+def GetBagHolders(color: str, holders: set[str]):
+    if color not in bag_groupings:
+        return
+    for k in bag_groupings[color].keys():
+        holders.add(k)
+        GetBagHolders(k, holders)
+
 
 def Part1():
-    pass
+    all_shiny_holders = set[str]()
+    if "shiny gold" not in bag_holdings:
+        all_shiny_holders.add("shiny gold")
+    GetBagHolders("shiny gold", all_shiny_holders)
+    print("Bags for Shiny Gold: {}".format(len(all_shiny_holders)))
+
+
+def GetBagsHeld(color: str) -> int:
+    total = 0
+    for k, count in bag_holdings.get(color, {}).items():
+        total += count + count * GetBagsHeld(k)
+    return total
 
 
 def Part2():
-    pass
+    print("Bags in Shiny Gold: {}".format(GetBagsHeld("shiny gold")))
 
 
 def main():
