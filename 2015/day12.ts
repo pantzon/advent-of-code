e7e6dfe59912b7d10034c54779e5f1533c7ee865
Pantz <pantz@pantz.online>
2024-12-13 23:24:41 -0500

    2015,2020 - Day 12

--- 2015/day12.ts
+++ 2015/day12.ts
@@ -2,9 +2,50 @@ import { readFileSync } from "fs";
 
 const data = readFileSync("./inputs/day12.txt", "utf-8");
 
-function part1() {}
+type Data = number | string | Data[] | { [key: string]: Data };
 
-function part2() {}
+const dataObj: Data = JSON.parse(data);
+
+function sum(d: Data): number {
+  if (typeof d === "number") {
+    return d;
+  }
+  if (typeof d === "string") {
+    return 0;
+  }
+  if (Array.isArray(d)) {
+    return d.reduce((acc: number, subData) => acc + sum(subData), 0);
+  }
+  return Object.values(d).reduce(
+    (acc: number, subData) => acc + sum(subData),
+    0
+  );
+}
+
+function part1() {
+  console.log(`Sum: ${sum(dataObj)}`);
+}
+
+function noRedSum(d: Data): number {
+  if (typeof d === "number") {
+    return d;
+  }
+  if (typeof d === "string") {
+    return 0;
+  }
+  if (Array.isArray(d)) {
+    return d.reduce((acc: number, subData) => acc + noRedSum(subData), 0);
+  }
+  const vals = Object.values(d);
+  if (vals.find((v) => v === "red")) {
+    return 0;
+  }
+  return vals.reduce((acc: number, subData) => acc + noRedSum(subData), 0);
+}
+
+function part2() {
+  console.log(`Sum: ${noRedSum(dataObj)}`);
+}
 
 console.log("DAY 12");
 part1();
--- 2020/day12.py
+++ 2020/day12.py
@@ -1,14 +1,90 @@
+from enum import Enum
+
+
 data = ""
 with open("inputs/day12.txt", "r") as f:
     data = f.read()
 
+INSTRUCTIONS: list[tuple[str, int]] = [
+    (l[0], int(l[1:])) for l in data.split("\n") if len(l) > 0
+]
+
+
+class Dir(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+
+def Move(direction: Dir, x: int, y: int, val: int) -> tuple[int, int]:
+    if direction == Dir.NORTH:
+        y += val
+    elif direction == Dir.EAST:
+        x += val
+    elif direction == Dir.SOUTH:
+        y -= val
+    elif direction == Dir.WEST:
+        x -= val
+    return x, y
+
+
+def P1FollowInstructions(dir: Dir, x: int, y: int) -> tuple[Dir, int, int]:
+    for r, val in INSTRUCTIONS:
+        if r == "N":
+            x, y = Move(Dir.NORTH, x, y, val)
+        elif r == "E":
+            x, y = Move(Dir.EAST, x, y, val)
+        elif r == "S":
+            x, y = Move(Dir.SOUTH, x, y, val)
+        elif r == "W":
+            x, y = Move(Dir.WEST, x, y, val)
+        elif r == "L":
+            dir = Dir((dir.value - (val / 90)) % 4)
+        elif r == "R":
+            dir = Dir((dir.value + (val / 90)) % 4)
+        elif r == "F":
+            x, y = Move(dir, x, y, val)
+        else:
+            print("UNKNOWN INSTRUCTION {} {}".format(r, val))
+    return dir, x, y
+
 
 def Part1() -> None:
-    pass
+    _, x, y = P1FollowInstructions(Dir.EAST, 0, 0)
+    print("Mahattan Distance: {}".format(abs(x) + abs(y)))
+
+
+def P2FollowInstructions(x: int, y: int, wx: int, wy: int) -> tuple[int, int]:
+    for r, val in INSTRUCTIONS:
+        if r == "N":
+            wx, wy = Move(Dir.NORTH, wx, wy, val)
+        elif r == "E":
+            wx, wy = Move(Dir.EAST, wx, wy, val)
+        elif r == "S":
+            wx, wy = Move(Dir.SOUTH, wx, wy, val)
+        elif r == "W":
+            wx, wy = Move(Dir.WEST, wx, wy, val)
+        elif r == "L":
+            val = int(val / 90)
+            for _ in range(0, val):
+                wx, wy = (-wy, wx)
+        elif r == "R":
+            val = int(val / 90)
+            for _ in range(0, val):
+                wx, wy = (wy, -wx)
+        elif r == "F":
+            for _ in range(0, val):
+                x, y = Move(Dir.NORTH, x, y, wy)
+                x, y = Move(Dir.EAST, x, y, wx)
+        else:
+            print("UNKNOWN INSTRUCTION {} {}".format(r, val))
+    return x, y
 
 
 def Part2() -> None:
-    pass
+    x, y = P2FollowInstructions(0, 0, 10, 1)
+    print("Mahattan Distance: {}".format(abs(x) + abs(y)))
 
 
 def main() -> None:
