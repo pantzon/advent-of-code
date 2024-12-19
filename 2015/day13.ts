9cb7b35e9927fbe0a754d37cd6fc9223369b8df1
Pantz <pantz@pantz.online>
2024-12-18 20:03:49 -0500

    2015,2020 - Day 13

--- 2015/day13.ts
+++ 2015/day13.ts
@@ -2,9 +2,63 @@ import { readFileSync } from "fs";
 
 const data = readFileSync("./inputs/day13.txt", "utf-8");
 
-function part1() {}
+const LINE_RE =
+  /^(?<A>[A-Za-z]+) would (?<dir>gain|lose) (?<score>[0-9]+) happiness units by sitting next to (?<B>[A-Za-z]+)\.$/;
 
-function part2() {}
+const pairs: Map<string, Map<string, number>> = new Map();
+
+for (const line of data.split("\n")) {
+  const m = LINE_RE.exec(line);
+  if (m && m.groups) {
+    const a = m.groups["A"];
+    const b = m.groups["B"];
+    const val =
+      (m.groups["dir"] === "gain" ? 1 : -1) * parseInt(m.groups["score"]);
+    if (!pairs.has(a)) {
+      pairs.set(a, new Map());
+    }
+    const aMap = pairs.get(a)!;
+    aMap.set(b, (aMap.get(b) ?? 0) + val);
+    if (!pairs.has(b)) {
+      pairs.set(b, new Map());
+    }
+    const bMap = pairs.get(b)!;
+    bMap.set(a, (bMap.get(a) ?? 0) + val);
+  }
+}
+
+function findBest(start: string, last: string, seated: Set<string>) {
+  if (seated.size === pairs.size) {
+    return pairs.get(start)!.get(last)!;
+  }
+  let maximum = Number.MIN_SAFE_INTEGER;
+  for (const a of [...pairs.keys()]) {
+    if (!seated.has(a)) {
+      const moreSeated = new Set(seated);
+      moreSeated.add(a);
+      const weight =
+        start && last
+          ? pairs.get(last)!.get(a)! + findBest(start, a, moreSeated)
+          : findBest(a, a, moreSeated);
+      if (weight > maximum) {
+        maximum = weight;
+      }
+    }
+  }
+  return maximum;
+}
+
+function part1() {
+  console.log(`Most Happiness: ${findBest("", "", new Set())}`);
+}
+
+function part2() {
+  for (const m of pairs.values()) {
+    m.set("Me", 0);
+  }
+  pairs.set("Me", new Map([...pairs.keys()].map((k) => [k, 0])));
+  console.log(`Most Happiness With Me: ${findBest("", "", new Set())}`);
+}
 
 console.log("DAY 13");
 part1();
--- 2020/day13.py
+++ 2020/day13.py
@@ -2,13 +2,47 @@ data = ""
 with open("inputs/day13.txt", "r") as f:
     data = f.read()
 
+START_TIME = 0
+BUSES: list[int] = []
+
+for line in data.split("\n"):
+    if line != "":
+        pieces = line.split(",")
+        if len(pieces) == 1:
+            START_TIME = int(pieces[0])
+        else:
+            BUSES = [int(num) if num != "x" else -1 for num in pieces]
+
 
 def Part1() -> None:
-    pass
+    first_bus = 0
+    min_wait = max(BUSES) + 1
+    for i in [b for b in BUSES if b > 0]:
+        wait = i - (START_TIME % i)
+        if wait < min_wait:
+            first_bus = i
+            min_wait = wait
+    print(
+        "First Bus is {} after {} mins: {}".format(
+            first_bus, min_wait, first_bus * min_wait
+        )
+    )
 
 
 def Part2() -> None:
-    pass
+    buses = [(b, i % b) for i, b in enumerate(BUSES) if b > 0]
+    t = buses[0][0]
+    c = buses[0][1]
+    for b in buses[1:]:
+        m = b[0] - b[1]
+        for i in range(0, b[0]):
+            # Fun modulo math, have to find the new offset
+            # with each added term.
+            if m == (t * i + c) % b[0]:
+                break
+        c = t * i + c
+        t = b[0] * t
+    print("In time order: {}".format(c))
 
 
 def main() -> None:
