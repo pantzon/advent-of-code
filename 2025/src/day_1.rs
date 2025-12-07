303be4159cb9395d8cd2798c532725c322f4e0f9
Pantz <pantz@pantz.online>
2025-12-06 21:16:34 -0500

    2025 - Day 1

--- 2025/src/day_1.rs
+++ 2025/src/day_1.rs
@@ -4,12 +4,42 @@ pub struct Day {
 
 impl crate::day_util::DaySolution for Day {
     fn part1(&self) -> String {
-      let len = self.data.len();
-      format!("Part 1: {len}")
+      let mut result: u16 = 0;
+      let mut pos = 50;
+      let instructions: Vec<&str> = self.data.split('\n').collect();
+      for inst in instructions {
+        if inst.len() == 0 {
+          continue;
+        }
+        let dir = inst.chars().nth(0).expect("has an initial character");
+        let count = inst[1..].parse::<i32>().unwrap();
+        pos += count * if dir == 'L' { -1 } else { 1 };
+        pos %= 100;
+        if pos == 0 {
+          result += 1;
+        }
+      }
+      format!("Part 1: {result}")
     }
 
     fn part2(&self) -> String {
-      let len = self.data.len();
-      format!("Part 2: {len}")
+      let mut result = 0;
+      let mut pos = 50;
+      let instructions: Vec<&str> = self.data.split('\n').collect();
+      for inst in instructions {
+        if inst.len() == 0 {
+          continue;
+        }
+        let dir = inst.chars().nth(0).expect("has an initial character");
+        let count = inst[1..].parse::<i32>().unwrap();
+        let orig_pos = pos;
+        result += count / 100;
+        pos += (count % 100) * if dir == 'L' { -1 } else { 1 };
+        if orig_pos != 0 && pos <= 0 || pos >= 100 {
+          result += 1;
+        }
+        pos = (100 + pos) % 100;
+      }
+      format!("Part 2: {result}")
     }
 }
