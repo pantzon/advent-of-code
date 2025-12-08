7c5a7074e1462723c59ef29bd03be7236b92191b
Pantz <pantz@pantz.online>
2025-12-07 21:51:29 -0500

    2025 - Day 4

--- 2025/src/day_4.rs
+++ 2025/src/day_4.rs
@@ -4,12 +4,66 @@ pub struct Day {
 
 impl crate::day_util::DaySolution for Day {
     fn part1(&self) -> String {
-      let len = self.data.len();
-      format!("Part 1: {len}")
+      let mut movable_rolls= 0;
+      let mut grid: Vec<Vec<char>> = Vec::new();
+      for line in self.data.split('\n') {
+        grid.push(line.chars().collect());
+      }
+      let height = grid.len();
+      let width = grid[0].len();
+      for y in 0..height {
+        for x in 0..width {
+          if grid[y][x] == '@' && count_neighbors(&grid, width, height, x, y) < 4 {
+            grid[y][x] = 'x';
+            movable_rolls += 1;
+          }
+        }
+      }
+      format!("Part 1: {movable_rolls}")
     }
 
     fn part2(&self) -> String {
-      let len = self.data.len();
-      format!("Part 2: {len}")
+      let mut grid: Vec<Vec<char>> = Vec::new();
+      for line in self.data.split('\n') {
+        grid.push(line.chars().collect());
+      }
+      let height = grid.len();
+      let width = grid[0].len();
+      let mut total_movable_rolls = 0;
+      loop {
+        let mut movable_rolls= 0;
+        for y in 0..height {
+          for x in 0..width {
+            if grid[y][x] == '@' && count_neighbors(&grid, width, height, x, y) < 4 {
+              grid[y][x] = 'x';
+              movable_rolls += 1;
+            }
+          }
+        }
+        if movable_rolls == 0 {
+          break;
+        }
+        total_movable_rolls += movable_rolls;
+        for y in 0..height {
+          for x in 0..width {
+            if grid[y][x] == 'x' {
+              grid[y][x] = '.';
+            }
+          }
+        }
+      }
+      format!("Part 2: {total_movable_rolls}")
     }
+}
+
+fn count_neighbors(grid: &Vec<Vec<char>>, width: usize, height: usize, x: usize, y: usize) -> i32 {
+    let mut neighbors = 0;
+    for i in (if y > 0 { y - 1 } else {0})..(if y < height - 1 { y + 2 } else { height }) {
+      for j in (if x > 0 { x - 1 } else {0})..(if x < width - 1 { x + 2 } else { width }) {
+        if grid[i][j] == '@' || grid[i][j] == 'x' {
+          neighbors += 1;
+        }
+      }
+    }
+    neighbors - 1  // Remove the center, i.e. itself.
 }
